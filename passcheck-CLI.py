91529f37e7cfc5ccbce2ea9c67030a499b4b2c5c
Justine Jae <bongalos.justinejaeg8@gmail.com>
2026-01-30 17:40:52 +0800

    CLI version done

--- passcheck-CLI.py
+++ passcheck-CLI.py
@@ -5,10 +5,29 @@ password = input("Enter Sample password: ")
 length = len(password)
 for i in password:
     if i in string.digits:
-        N+=10
+        N+=len(string.digits)
         break
 for i in password:
-    if i in string.digits:
+    if i in string.ascii_lowercase:
+        N+=len(string.ascii_lowercase)
+
+for i in password:
+    if i in string.ascii_uppercase:
+        N+=len(string.ascii_uppercase)
+for i in password:
+    if i in string.punctuation:
+        N+=len(string.punctuation)
+
+entropy =math.log2(N**length)
+print (entropy)
+if entropy <= 72:
+    print("weak ahh password")
+elif entropy <=95:
+    print("password pretty mid like u")
+elif entropy <= 115:
+    print("strong password")
+else:
+    print("ts ahh pass so tuff fr")
 
 
 
